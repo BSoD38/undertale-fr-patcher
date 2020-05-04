68337f7950fce15bac7b95647da6889bc7e64b1e
Liam Powell <bsod1998@gmail.com>
2020-05-04 21:43:13 +0200

    Patching is almost functional + patcher changes

--- src/Utils/Patcher.js
+++ src/Utils/Patcher.js
@@ -1,116 +1,138 @@
 // Created by Alcaro, and edited by the UTFR Team.
-const crc32 = require("Checksum");
-
 module.exports = class Patcher {
     /**
-     * @param Buffer data
-     * @param Buffer patch
-     * @returns {Buffer}
+     * @param bytes
+     * @returns {number}
      */
-    static applyBps({buf: data}, {buf: patch}) {
-        let i;
-        let patchpos = 0;
-
-        function u8() {
-            return patch[patchpos++];
+    static crc32(bytes) {
+        let c;
+        const crcTable = [];
+        for (let n = 0; n < 256; n++) {
+            c = n;
+            for (let k = 0; k < 8; k++) {
+                c = c & 1 ? 0xedb88320 ^ (c >>> 1) : c >>> 1;
+            }
+            crcTable[n] = c;
         }
 
-        function u32at(pos) {
-            return (
-                ((patch[pos + 0] << 0) |
-                    (patch[pos + 1] << 8) |
-                    (patch[pos + 2] << 16) |
-                    (patch[pos + 3] << 24)) >>>
-                0
-            );
+        let crc = 0 ^ -1;
+        for (let i = 0; i < bytes.length; i++) {
+            crc = (crc >>> 8) ^ crcTable[(crc ^ bytes[i]) & 0xff];
         }
+        return (crc ^ -1) >>> 0;
+    }
 
-        function decode() {
-            let ret = 0;
-            let sh = 0;
-            while (true) {
-                const next = u8();
-                ret += (next ^ 0x80) << sh;
-                if (next & 0x80) {
-                    return ret;
+    /**
+     * @returns {Buffer}
+     * @param data
+     * @param patch
+     */
+    static applyBps(data, patch) {
+        return new Promise((resolve, reject) => {
+            function u8() {
+                return patch[patchpos++];
+            }
+
+            function u32at(pos) {
+                return (
+                    ((patch[pos + 0] << 0) |
+                        (patch[pos + 1] << 8) |
+                        (patch[pos + 2] << 16) |
+                        (patch[pos + 3] << 24)) >>>
+                    0
+                );
+            }
+
+            function decode() {
+                let ret = 0;
+                let sh = 0;
+                while (true) {
+                    const next = u8();
+                    ret += (next ^ 0x80) << sh;
+                    if (next & 0x80) {
+                        return ret;
+                    }
+                    sh += 7;
                 }
-                sh += 7;
             }
-        }
 
-        function decodes() {
-            const enc = decode();
-            let ret = enc >> 1;
-            if (enc & 1) {
-                ret = -ret;
+            function decodes() {
+                const enc = decode();
+                let ret = enc >> 1;
+                if (enc & 1) {
+                    ret = -ret;
+                }
+                return ret;
             }
-            return ret;
-        }
 
-        if (u8() !== 0x42 || u8() !== 0x50 || u8() !== 0x53 || u8() !== 0x31) {
-            throw "not a BPS patch";
-        }
-        if (decode() !== data.length) {
-            throw "wrong input file";
-        }
-        if (crc32(data) !== u32at(patch.length - 12)) {
-            throw "wrong input file";
-        }
+            let i;
+            let patchpos = 0;
 
-        const out = new Uint8Array(decode());
-        let outpos = 0;
-
-        const metalen = decode();
-        patchpos += metalen; // can't join these two, JS reads patchpos before calling decode
-
-        const SourceRead = 0;
-        const TargetRead = 1;
-        const SourceCopy = 2;
-        const TargetCopy = 3;
-
-        let inreadpos = 0;
-        let outreadpos = 0;
-
-        while (patchpos < patch.length - 12) {
-            const thisinstr = decode();
-            const len = (thisinstr >> 2) + 1;
-            const action = thisinstr & 3;
-
-            switch (action) {
-                case SourceRead:
-                    {
-                        for (i = 0; i < len; i++) {
-                            out[outpos] = data[outpos];
-                            outpos++;
-                        }
-                    }
-                    break;
-                case TargetRead:
-                    {
-                        for (i = 0; i < len; i++) {
-                            out[outpos++] = u8();
-                        }
-                    }
-                    break;
-                case SourceCopy:
-                    {
-                        inreadpos += decodes();
-                        for (i = 0; i < len; i++) {
-                            out[outpos++] = data[inreadpos++];
-                        }
-                    }
-                    break;
-                case TargetCopy:
-                    {
-                        outreadpos += decodes();
-                        for (i = 0; i < len; i++) {
-                            out[outpos++] = out[outreadpos++];
-                        }
-                    }
-                    break;
+            if (u8() !== 0x42 || u8() !== 0x50 || u8() !== 0x53 || u8() !== 0x31) {
+                reject("Patch corrompu");
             }
-        }
+            if (decode() !== data.length) {
+                reject("Fichier de données incompatible");
+            }
+            if (this.crc32(data) !== u32at(patch.length - 12)) {
+                reject("Fichier de données incompatible");
+            }
 
-        return new Buffer(out);
+            const out = new Uint8Array(decode());
+            let outpos = 0;
+
+            const metalen = decode();
+            patchpos += metalen; // can't join these two, JS reads patchpos before calling decode
+
+            const SourceRead = 0;
+            const TargetRead = 1;
+            const SourceCopy = 2;
+            const TargetCopy = 3;
+
+            let inreadpos = 0;
+            let outreadpos = 0;
+
+            while (patchpos < patch.length - 12) {
+                const thisinstr = decode();
+                const len = (thisinstr >> 2) + 1;
+                const action = thisinstr & 3;
+
+                switch (action) {
+                    case SourceRead:
+                        {
+                            for (i = 0; i < len; i++) {
+                                out[outpos] = data[outpos];
+                                outpos++;
+                            }
+                        }
+                        break;
+                    case TargetRead:
+                        {
+                            for (i = 0; i < len; i++) {
+                                out[outpos++] = u8();
+                            }
+                        }
+                        break;
+                    case SourceCopy:
+                        {
+                            inreadpos += decodes();
+                            for (i = 0; i < len; i++) {
+                                out[outpos++] = data[inreadpos++];
+                            }
+                        }
+                        break;
+                    case TargetCopy:
+                        {
+                            outreadpos += decodes();
+                            for (i = 0; i < len; i++) {
+                                out[outpos++] = out[outreadpos++];
+                            }
+                        }
+                        break;
+                }
+            }
+
+            resolve(Buffer.from(out));
+        });
     }
 }
--- src/pagelogic.js
+++ src/pagelogic.js
@@ -1,17 +1,53 @@
 const { dialog } = require("electron").remote;
 const fs = require("fs").promises;
+const fsConstants = require("fs").constants;
+const Patcher = require("./Utils/Patcher");
 
 let selectedOriginalData;
+let dataBuffer;
+let patchBuffer;
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("select-orig-file").addEventListener("click", async () => {
-        selectedOriginalData = await dialog.showOpenDialog({
+        const selectedOriginalData = await dialog.showOpenDialog({
             properties: ["openFile"],
             filters: [
                 {name: "Données d'Undertale", extensions: ["win", "unx", "ios"]}
             ]
         });
+
         if (selectedOriginalData.filePaths[0]) {
-            console.log(await fs.readFile(selectedOriginalData.filePaths[0]));
+            dataBuffer = await fs.readFile(selectedOriginalData.filePaths[0]);
         }
     });
-})
+
+    document.getElementById("select-patch").addEventListener("click", async () => {
+        selectedOriginalData = await dialog.showOpenDialog({
+            properties: ["openFile"],
+            filters: [
+                {name: "Fichier de patch", extensions: ["bps"]}
+            ]
+        });
+
+        if (selectedOriginalData.filePaths[0]) {
+            patchBuffer = await fs.readFile(selectedOriginalData.filePaths[0]);
+        }
+    });
+
+    document.getElementById("patch").addEventListener("click", async () => {
+        if (dataBuffer instanceof Buffer && patchBuffer instanceof Buffer) {
+            const backupFilename = `${selectedOriginalData.filePaths[0]}.bak`;
+            try {
+                await fs.access(backupFilename, fsConstants.F_OK);
+            } catch (e) {
+                await fs.copyFile(selectedOriginalData.filePaths[0], `${selectedOriginalData.filePaths[0]}.bak`);
+            }
+            try {
+                const resultBuffer = Patcher.applyBps(dataBuffer, patchBuffer);
+                await fs.writeFile(selectedOriginalData.filePaths[0], resultBuffer);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+});
